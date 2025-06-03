21bd7b0fedcdcf5c2a80908cb32c12862f11b607
b-swist <bswist@protonmail.com>
2025-06-04 00:14:40 +0200

    update bash config

--- home/bash.nix
+++ home/bash.nix
@@ -1,13 +1,30 @@
-{...}: {
+{config, ...}: {
   programs.bash = {
     enable = true;
+
+    historyFile = "${config.xdg.stateHome}/bash/history";
+    historyIgnore = ["ls"];
+    historyControl = [
+      "ignoreboth"
+      "erasedups"
+    ];
+
     initExtra = ''
       PS1="\w > "
     '';
-    historyControl = ["erasedups"];
+
+    shellOptions = [
+      "autocd"
+      "cdspell"
+      "checkwinsize"
+      "extglob"
+      "globstar"
+      "histappend"
+    ];
     shellAliases = {
       ll = "ls -lAh";
       ".." = "cd ..";
+      wget = "wget --hsts-file=${config.xdg.dataHome}/wget-hsts";
     };
   };
 }
