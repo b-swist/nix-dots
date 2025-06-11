ce9c6a038c6d5a23ec1024acaabc986906feb506
b-swist <bswist@protonmail.com>
2025-06-11 19:25:50 +0200

    update systemd-boot config

--- system/desktop/configuration.nix
+++ system/desktop/configuration.nix
@@ -16,7 +16,11 @@
   };
 
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      editor = false;
+      configurationLimit = 5;
+    };
     efi.canTouchEfiVariables = true;
   };
 
